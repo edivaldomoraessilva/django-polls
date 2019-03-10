fcc56650aff5eae3453b6b3c28a06057f442da5f
Edivaldo Moraes Silva <47981838+edivaldomoraessilva@users.noreply.github.com>
2019-03-10 15:23:08 -0300

    create set class and define active feature_custom_quereysets

--- ambientes_virtuais/django_ecommerce/src/products/views.py
+++ ambientes_virtuais/django_ecommerce/src/products/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -9,10 +11,11 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
-   # def get_context_data(self, *args, **kwargs):
-   #    context = super(ProductListView, self).get_context_data(*args, **kwargs)
-   #   print(context)
-   #  return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 #Function Based View
 def product_list_view(request):
@@ -22,22 +25,30 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
-    #Class Based View
+#Class Based View
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!cbv")
+        return instance
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
-    #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
-    queryset = Product.objects.all()
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
+        raise Http404("Esse produto não existe!fbv")
+
     context = {
         'object': instance
     }
